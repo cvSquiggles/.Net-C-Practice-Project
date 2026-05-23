756e5d4fa0fbfa541a865cab640b6fab4e959d61
cvSquiggles <jones.steven.alexander@gmail.com>
2026-05-23 15:52:32 -0400

    Intial local commit, tinkering with C# data structures.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Hello, Worldzzz!");
+
+List<string> myGroceryList = ["apple", "milk"];
+
+
+IEnumerable<string> ieString = myGroceryList;
+
+//foreach(string x in ieString)
+//{
+    //Console.WriteLine(x);
+//}
+
+string[,,] multidimensionalArray = {
+    
+        {
+            {"apple", "2 Dollars"}, 
+            {"bananas", "3 Dollars"}
+        },
+        {
+            {"milk", "Free with purchase of soda"}, 
+            {"soda", "9 Dollars"}
+        }
+    };
+
+//Console.WriteLine(multidimensionalArray[1,0,1]);
+
+Dictionary<string, decimal> groceryPrices = new Dictionary<string, decimal>();
+
+groceryPrices["Cheese"] = 5.29m;
+
+Console.WriteLine(groceryPrices["Cheese"]);
