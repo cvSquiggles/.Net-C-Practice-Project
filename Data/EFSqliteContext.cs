bb34d707ff57dd43899632b1fb72a0b224bedaed
cvSquiggles <jones.steven.alexander@gmail.com>
2026-05-28 03:43:22 -0400

    Removed redundant IConfiguration _config from EFSqliteContext.cs that I accidentally left in.

--- Data/EFSqliteContext.cs
+++ Data/EFSqliteContext.cs
@@ -16,8 +16,6 @@ public class EFSqliteContext : DbContext
 {
     //I'm learning that my Library class and it's methods are redundant, since EntityFramework handles those things, I shouldn't need explicit methods. Going to try dropping them and see if actions work directly through the DBContext WebApp service.
     public EFSqliteContext(DbContextOptions<EFSqliteContext> options) : base(options) {} //My previous constructor used DbContextOptionsBuilder, but we don't need the builder, we just need the options themselves....we just extned the DbContext.base(options)
-
-    private IConfiguration _config = new ConfigurationBuilder().AddJsonFile("AppSettings.json").Build();
    
     public DbSet<Book> Book { get; set; }
     public DbSet<Genre> Genre { get; set; }
